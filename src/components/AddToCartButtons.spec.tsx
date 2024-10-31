db7f3335b33128be36dd67d9d11d5be707f93bcc
Czino <eric@czino.de>
2024-10-31 16:44:11 +0100

    Minor fixes

--- src/components/AddToCartButtons.spec.tsx
+++ src/components/AddToCartButtons.spec.tsx
@@ -4,12 +4,17 @@ import { AddToCartButtons } from './AddToCartButtons'
 describe('AddToCartButtons', () => {
     const npub = 'npub'
     const productId = 'productId'
+    const url = 'url'
     it('renders correctly when disabled', () => {
         const { asFragment } = render(<AddToCartButtons npub={npub} productId={productId} enabled={false} />)
         expect(asFragment()).toMatchSnapshot()
     })
     it('renders correctly when enabled', () => {
         const { asFragment } = render(<AddToCartButtons npub={npub} productId={productId} enabled={true} />)
+        expect(asFragment()).toMatchSnapshot()
+    })
+    it('renders correctly with url', () => {
+        const { asFragment } = render(<AddToCartButtons npub={npub} url={url} productId={productId} enabled={true} />)
         expect(asFragment()).toMatchSnapshot()
     })
 })
--- src/components/AddToCartButtons.tsx
+++ src/components/AddToCartButtons.tsx
@@ -13,18 +13,24 @@ const buttonClassName =
     'flex justify-center w-full bg-dark-2 border border-highlight-1 p-3 rounded text-xl text-light-1'
 export const AddToCartButtons = ({ productId, npub, url, enabled = false }: Props) => {
     if (!enabled) return <h3>Out of stock</h3>
+    const cols = url ? 'grid-cols-3' : 'grid-cols-2'
     return (
         <div className="grid grid-cols-1 gap-2">
             <h3 className="text-xl">Get in touch:</h3>
-            <div className="grid grid-cols-3 gap-2">
-                <a href={`https://njump.me/${npub}`} className={buttonClassName}>
+            <div className={`grid gap-2 ${cols}`}>
+                <a href={`https://njump.me/${npub}`} target="_blank" className={buttonClassName} rel="noreferrer">
                     <NostrLogo className="h-10" />
                 </a>
-                <a href={`https://plebeian.market/product/${productId}`} className={buttonClassName}>
+                <a
+                    href={`https://plebeian.market/product/${productId}`}
+                    target="_blank"
+                    className={buttonClassName}
+                    rel="noreferrer"
+                >
                     <PlebeianMarketLogo className="h-10 text-light-1 fill-light-1" />
                 </a>
                 {!!url && (
-                    <a href={url} className={buttonClassName}>
+                    <a href={url} target="_blank" className={buttonClassName} rel="noreferrer">
                         <MailIcon className="h-10 stroke-light-1" />
                     </a>
                 )}
